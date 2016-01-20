37be69fa810fa832d5db3f9e096cfbd5381aed62
Thomas Hallock <thomas@1stdibs.com>
2016-01-19 19:00:43 -0500

    assuming commonjs

--- backbone.js
+++ backbone.js
@@ -5,42 +5,21 @@
 //     For all details and documentation:
 //     http://backbonejs.org
 
-(function(factory) {
-
   // Establish the root object, `window` (`self`) in the browser, or `global` on the server.
   // We use `self` instead of `window` for `WebWorker` support.
-  var root = (typeof self == 'object' && self.self === self && self) ||
-            (typeof global == 'object' && global.global === global && global);
+  var root = global;
 
-  // Set up Backbone appropriately for the environment. Start with AMD.
-  if (typeof define === 'function' && define.amd) {
-    define(['underscore', 'jquery', 'exports'], function(_, $, exports) {
-      // Export global even in AMD case in case this script is loaded with
-      // others that may still expect a global Backbone.
-      root.Backbone = factory(root, exports, _, $);
-    });
+  // assume CommonJS
+  var _ = require('underscore'), $;
+  try { $ = require('jquery'); } catch (e) {}
 
-  // Next for Node.js or CommonJS. jQuery may not be needed as a module.
-  } else if (typeof exports !== 'undefined') {
-    var _ = require('underscore'), $;
-    try { $ = require('jquery'); } catch (e) {}
-    factory(root, exports, _, $);
-
-  // Finally, as a browser global.
-  } else {
-    root.Backbone = factory(root, {}, root._, (root.jQuery || root.Zepto || root.ender || root.$));
-  }
-
-})(function(root, Backbone, _, $) {
+  var Backbone = module.exports = {};
 
   // Initial Setup
   // -------------
 
   // Create a local reference to a common array method we'll want to use later.
   var slice = Array.prototype.slice;
-
-  // Current version of the library. Keep in sync with `package.json`.
-  Backbone.VERSION = '1.2.3';
 
   // For Backbone's purposes, jQuery, Zepto, Ender, or My Library (kidding) owns
   // the `$` variable.
@@ -531,6 +510,3 @@
     };
   };
 
-  return Backbone;
-
-});
